1b38df5ed2cb6a900e4ff31eb02ab1e5b1807adb
Pedro Henrique Souza De Queiroz <38128442813@info.local>
2019-11-04 15:26:59 -0200

    up

---
+++ Database/SQL_DLL.sql
@@ -0,0 +1,41 @@
+create database  T_Roman
+
+use T_Roman
+
+create table Temas(
+idTema int primary key identity
+,nome varchar(255) not null
+,ativo bit default (1)
+)
+
+create table Permissao(
+idPermissao int primary key identity
+,nome varchar(255) not null
+)
+
+create table Projetos(
+idProjeto int primary key identity
+,nome varchar (255) not null
+,idTema int foreign key references Temas (idTema)
+)
+
+create table usuarios (
+Nome varchar(255) not null
+,Email Varchar(255) not null unique	
+,Senha Varchar (255) not null 
+,Foto text  default('http://cdn.onlinewebfonts.com/svg/img_95941.png') not null
+)
+alter table usuarios add idPermissao int foreign key references Permissao (idPermissao)
+
+insert into Temas (nome) values ('sei la'),('sei la 2')
+select * from Temas
+
+insert into Permissao (nome) values ('Professor'), ('Aluno')
+select * from Permissao
+
+insert into Projetos(nome,idTema) values ('Projeto 2', 2)
+select * from Projetos
+
+insert into usuarios (Nome ,Email,Senha,idPermissao) values ('schorsch','schorsch@gmail.com','password',1),('Pedro','Pedro@gmail.com','password',1)
+
+select * from usuarios
